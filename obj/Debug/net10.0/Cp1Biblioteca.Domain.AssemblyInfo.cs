03d2121782dc783c43b097673e0a6ce0b6a4ffd7
Felipe Beatriz <felipe.beatriz199@gmail.com>
2026-03-13 11:36:03 -0300

    feat: add MER image

--- obj/Debug/net10.0/Cp1Biblioteca.Domain.AssemblyInfo.cs
+++ obj/Debug/net10.0/Cp1Biblioteca.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cp1Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79ba018fc98c8204e7b6370063b79e10cbecd69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0931c9a9eae90acb46cee78c5b26c21af51483")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cp1Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cp1Biblioteca.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
