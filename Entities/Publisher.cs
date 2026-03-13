4f6459e98b702fa916601d4a43296a7d37aabb6e
Felipe Beatriz <felipe.beatriz199@gmail.com>
2026-03-13 12:56:35 -0300

    feat: docs folder with MER image

--- Entities/Publisher.cs
+++ Entities/Publisher.cs
@@ -7,7 +7,7 @@ public class Publisher : BaseEntity
     public string Name { get; private set; }
     public List<Book> Books { get; private set; }
 
-    public Publisher(string name, string country)
+    public Publisher(string name)
     {
         Name = name;
     }
